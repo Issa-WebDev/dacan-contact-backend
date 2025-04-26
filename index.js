8f5b51b1a4609db4d9f70086480a679853538340
Issa-WebDev <kissadev10@gmail.com>
2025-04-26 16:09:00 +0100

    dacan contact backend

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import nodemailer from "nodemailer"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/contact", async (req, res) => {
+  const { name, email, phone, subject, message } = req.body;
+
+  if (!name || !email || !phone || !subject) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  try {
+    // transporter
+    const transporter = nodemailer.createTransport({
+      service: "gmail", 
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    // mail content
+
+    const mailOptions = {
+      from: email,
+      to: process.env.MAIL_TO,
+      subject: `Contact: ${subject}`,
+      html: `
+      <p><strong>Nom:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Numéro:</strong> ${phone}</p>
+      <p><strong>Sujet:</strong> ${subject}</p>
+      <p><strong>Message:</strong> ${message}</p>
+    `,
+    };
+
+    // send mail
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: "Message sent successfully" });
+  } catch (error) {
+    console.error("Error sending mail:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
